84a4bf91510cb1742dcc73ae1d2e9b9fd840120e
Valentin Makov <valentin.makov@gmail.com>
2021-01-24 17:27:31 +0300

    Thunk import fixed

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
-import {thunk} from 'redux-thunk'
+import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {todos} from './todos/reducer'
 
