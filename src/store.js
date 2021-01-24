415269caad8b30ce10a6f08952ef82a5158be5ef
Valentin Makov <valentin.makov@gmail.com>
2021-01-24 16:21:39 +0300

    Persist reducer added

--- src/store.js
+++ src/store.js
@@ -1,9 +1,20 @@
 import {createStore, combineReducers} from 'redux'
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 import {todos} from './todos/reducer'
 
 const reducers = {
     todos,
 }
-const rootReucer = combineReducers(reducers)
 
-export const configureStore = () => createStore(rootReucer)
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+}
+
+const rootReducer = combineReducers(reducers)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const configureStore = () => createStore(persistedReducer)
