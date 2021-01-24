2f5221a5cae080d10641984ed508c4301ace804f
Valentin Makov <valentin.makov@gmail.com>
2021-01-24 13:44:28 +0300

    Components linked

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import NewTodoForm from './NewTodoForm'
 import TodoListItem from './TodoListItem'
 import './TodoList.css'
 
-const TodoList = ({todos}) => (
+const TodoList = ({todos = [{text: 'Hello'}]}) => (
     <div className={'list-wrapper'}>
+        <NewTodoForm />
         {todos.map(todo => <TodoListItem todo={todo}/>)}
     </div>
 )
