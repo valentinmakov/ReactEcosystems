146132f67160158a421a215a3ec7d7af8ca987ce
Valentin Makov <valentin.makov@gmail.com>
2021-01-28 20:55:56 +0300

    Styled components added

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,15 +1,19 @@
 import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
+import styled from 'styled-components'
 import NewTodoForm from './NewTodoForm'
 import TodoListItem from './TodoListItem'
 import {loadTodo, removeTodoRequest, markTodoAsCompletedRequest} from './thunks'
 import {
-    getTodos,
     getIsLoading,
     getIncompleteTodos,
     getCompletedTodos,
 } from './selectors'
-import './TodoList.css'
+
+const ListWrapper = styled.div`
+    max-width: 700px;
+    margin: auto;
+`
 
 const TodoList = ({incompleteTodos, completedTodos, onRemovePressed, onCompletedPressed, startLoadingTodos, isLoading}) => {
     useEffect(() => {
@@ -18,7 +22,7 @@ const TodoList = ({incompleteTodos, completedTodos, onRemovePressed, onCompleted
 
     const loadingMessage = <div>Loading todos...</div>
     const content = (
-        <div className={'list-wrapper'}>
+        <ListWrapper>
             <NewTodoForm />
             <h3>Incomplete:</h3>
             {incompleteTodos.map(todo =>
@@ -34,7 +38,7 @@ const TodoList = ({incompleteTodos, completedTodos, onRemovePressed, onCompleted
                     onCompletedPressed={onCompletedPressed}
                     onRemovePressed={onRemovePressed}/>
             )}
-        </div>
+        </ListWrapper>
     )
     return isLoading ? loadingMessage : content
 }
