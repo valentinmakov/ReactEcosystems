fe5cde747c7cf64455b823605f1d3a9ed2dc75e0
Valentin Makov <valentin.makov@gmail.com>
2021-01-31 16:03:20 +0300

    App style updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import {hot} from 'react-hot-loader'
 import TodoList from './todos/TodoList'
 
+const AppContainer = styled.div`
+    margin: 1rem;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #222;
+    width: 100vw;
+    height: 100vh;
+`
+
 const App = () => (
-    <div>
+    <AppContainer>
         <TodoList />
-    </div>
+    </AppContainer>
 )
 
 export default hot(module)(App)
